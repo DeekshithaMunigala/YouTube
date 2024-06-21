b43a5815b604c49e256de4b5559cf3aca26127fc
Munigala Deekshitha <145666932+DeekshithaMunigala@users.noreply.github.com>
2024-06-21 10:03:04 +0530

    Update Contants.jsx

--- src/utils/Contants.jsx
+++ src/utils/Contants.jsx
@@ -1,5 +1,4 @@
-// const GOOGLE_API_KEY = "AIzaSyBMYjR-6tIFYkPfn2J2RR-8HIDqDGWPZqc";
-const GOOGLE_API_KEY = "AIzaSyABs0Ct_3zL1LZKrGnR1kUHXXcu1C8BWuk";
+const GOOGLE_API_KEY = "AIzaSyBMYjR-6tIFYkPfn2J2RR-8HIDqDGWPZqc";
 
 export const YOUTUBE_VIDEO_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
